e79fbceaca0d2df9de298b4def4842a9ededea0a
DaKu <do.holyada@gmail.com>
2022-09-17 23:46:16 +0300

    3,4

--- 3.py
+++ 3.py
@@ -5,3 +5,25 @@
 # Пример:
 
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+import math
+
+a = 3
+b = -2
+c = 8
+
+def find_roots(a, b, c):
+    D = b ^ 2 - 4*a*c
+    roots = []
+    if D < 0: 
+        roots.append('Нет корней')
+        return roots
+    x1 = (-b + math.sqrt(D)) / 2*a
+    roots.append(x1)
+    if D > 0: 
+        x2 = (-b - math.sqrt(D)) / 2*a
+        roots.append(x2)
+    return roots   
+
+print(find_roots(a, b, c))
+print(type(find_roots(a, b, c)))
